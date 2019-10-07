1305776fbe85772772ddb66234102606721a9eca
Cruzverme <grossmino@hotmail.com>
2019-10-07 18:03:32 -0300

    Add Controller Costumer

---
+++ app/Http/Controllers/CostumerController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Costumer;
+
+class CostumerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $costumers = Costumer::all();
+        return view('costumers/index',compact('costumers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('costumers/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        // $costumer = new Costumer;
+        // $costumer->name = $request->name;
+        // $costumer->street = $request->street;
+        // $costumer->streetNumber = $request->streetNumber;
+        // $costumer->neighborhood = $request->neighborhood;
+
+        // $costumer->save();
+        $validateData = $request->validate([
+            'name' => 'required|max:255',
+            'street' => 'required',
+            'streetNumber' => 'required|numeric',
+            'neighborhood' => 'required'
+        ]);
+        $costumer = Costumer::create($validateData);
+        return redirect('/costumers')->with('message', 'Product created successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $costumer = Costumer::findOrFail($id);
+
+        return view('costumers/edit', compact('costumer'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $validateData = $request->validate([
+            'name' => 'required|max:255',
+            'street' => 'required',
+            'streetNumber' => 'required|numeric',
+            'neighborhood' => 'required'
+        ]);
+
+        Costumer::whereId($id)->update($validateData);
+
+        return redirect('/costumers')->with('message', 'Product is successfully updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $costumer = Costumer::findOrFail($id);
+        $costumer->delete();
+
+        return redirect('/costumers')->with('success', 'Costumer is successfully deleted');
+    }
+}
